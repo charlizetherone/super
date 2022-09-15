2464adb00471106abee66d8bc8709da967b09bf2
charlizetherone <113601475+charlizetherone@users.noreply.github.com>
2022-09-15 11:25:54 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector("ul");
+const menu = document.querySelector(".hamburger");
+const reveal = document.querySelector(".reveal")
+
+function change() {
+    hamburger.classList.toggle("reveal");
+
+}
